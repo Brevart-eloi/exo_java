4dea336160ffc7f0fef811de2caeb8cad5cb1178
Eloi <tuo@gmail.com>
2024-11-20 11:58:21 +0100

    deuxième commit

--- exo4/exo4.js
+++ exo4/exo4.js
@@ -1,6 +1,6 @@
 var mavarstr = 'bou';
 var mavarint = prompt('entrez une valeur',100);
-if (mavarstr == 'bou'&& mavarint != 200 || mavarint<=100){
+if (mavarstr == 'bou' && mavarint != 200 || mavarint<=100){
     alert('je suis le cas 1');
 }else{
     alert('je suis le cas 2')// cas 2 si la valeur entrer est 200
--- exo5/exo5.js
+++ exo5/exo5.js
@@ -1,3 +1,10 @@
-varstr = 'mama'
-mavartaille300pxlargeur = 
-alert(mavartaille300pxlargeur+varstr)
+// Fonction qui vérifie la largeur de la fenêtre
+function checkWindowSize() {
+    if (window.innerWidth < 300) {
+        alert('fenêtre inférieure à 300px !');
+    }
+}
+// Ajout d'un écouteur d'événement pour détecter les redimensionnements
+window.addEventListener('resize', checkWindowSize);
+// Vérifie immédiatement si la fenêtre est déjà inférieure à 300px
+checkWindowSize();
