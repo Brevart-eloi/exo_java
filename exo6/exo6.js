1c473ce7841c51597a70e680a54769b9831a3038
brevart <eloi.brevart@gmail.com>
2024-12-08 13:27:06 +0000

    qd

--- exo6/exo6.js
+++ exo6/exo6.js
@@ -8,3 +8,13 @@ for(let i=1;i<mavarint;i++)
 
 
 alert(resultat)
+// Fonction qui vérifie la largeur de la fenêtre
+function checkWindowSize() {
+    if (window.innerWidth < 300) {
+        alert('fenêtre inférieure à 300px !');
+    }
+}
+// Ajout d'un écouteur d'événement pour détecter les redimensionnements
+window.addEventListener('resize', checkWindowSize);
+// Vérifie immédiatement si la fenêtre est déjà inférieure à 300px
+checkWindowSize();
